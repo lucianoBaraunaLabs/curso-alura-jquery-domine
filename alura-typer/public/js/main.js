7b0699fc14b80fc278870ba61a777ecec7246b3b
= <baraunaluciano@gmail.com>
2016-11-06 16:22:37 -0200

    Finalizado Cap_01

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,1 +1,5 @@
-alert('ola')
+// Contando as palavras e colocando na tela
+var frase = $('.frase').text();
+var numPalavras = frase.split(" ").length;
+var tamanhoFrase = $('#tamanho-frase');
+tamanhoFrase.text(numPalavras);
