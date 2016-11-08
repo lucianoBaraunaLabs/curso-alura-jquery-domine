6f2bc319011df9b3e333548ecf1b4a6f1e8cf920
LucianoBarauna <baraunaluciano@gmail.com>
2016-11-07 23:09:14 -0200

    Finalizado cap_04

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,30 +1,63 @@
+var tempoInicial = $('#tempo-digitacao').text();
+var campo = $('.campo-digitacao');
+
+$(function(){
+  atualizaTamanhoFrase();
+  inicializaContadores();
+  inicializaCronometro();
+  $('#botao-reiniciar').click(reiniciaJogo);
+
+});
+
 // Contando as palavras e colocando na tela
-var frase = $('.frase').text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $('#tamanho-frase');
-tamanhoFrase.text(numPalavras);
+function atualizaTamanhoFrase() {
+  var frase = $('.frase').text();
+  var numPalavras = frase.split(" ").length;
+  var tamanhoFrase = $('#tamanho-frase');
+  tamanhoFrase.text(numPalavras);
+};
 
 // Restultado das palavras digitadas realtime
-var campo = $('.campo-digitacao');
-campo.on('input', function () {
-  var conteudo = campo.val();
-  var qtdPalavras = conteudo.split(/\S+/).length - 1; // Busca qualquer caracter, exceto espaço vazio
-  $("#contador-palavras").text(qtdPalavras);
-  console.log("digito");
+function inicializaContadores() {
+  campo.on('input', function () {
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length - 1; // Busca qualquer caracter, exceto espaço vazio
+    $("#contador-palavras").text(qtdPalavras);
+    console.log("digito");
 
-  var qtdCaracteres = conteudo.length;
-  $("#contador-caracteres").text(qtdCaracteres);
-});
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+  });
+};
 
 // Cronometro de tempo restante
-var tempoRestante = $('#tempo-digitacao').text();
-campo.one('focus',function () { // Funcao one só escuta o evento uma única vez
-  var cronometroID = setInterval(function () {
-                        tempoRestante--; // subtrai do tempo
-                        $('#tempo-digitacao').text(tempoRestante);
-                        if (tempoRestante < 1) {
-                            campo.attr("disabled", true);
-                            clearInterval(cronometroID);
-                        }
-                    }, 1000);
-});
+function inicializaCronometro() {
+  var tempoRestante = $('#tempo-digitacao').text();
+  campo.one('focus',function () { // Funcao one só escuta o evento uma única vez
+    $('#botao-reiniciar').attr('disabled', true);
+    var cronometroID = setInterval(function () {
+      tempoRestante--; // subtrai do tempo
+      $('#tempo-digitacao').text(tempoRestante);
+      if (tempoRestante < 1) {
+        campo.attr("disabled", true);
+        clearInterval(cronometroID);
+        finalizaJogo();
+      }
+    }, 1000);
+  });
+};
+
+// Resetar cronometro
+function reiniciaJogo() {
+  campo.attr('disabled', false);
+  campo.val("");
+  $('#contador-palavras').text(0);
+  $('#contador-caracteres').text(0);
+  $('#tempo-digitacao').text(tempoInicial);
+  inicializaCronometro();
+}
+
+// Finaliza jogo
+function finalizaJogo(){
+    $('#botao-reiniciar').attr('disabled', false);
+}
